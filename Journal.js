eb33636a51f541a93c777d2b6f1276fb41ec393c
dedrickj1 <dedrickjackson11@gmail.com>
2024-12-05 21:14:57 -0600

    Update for December 5th

---
+++ Journal.js
@@ -0,0 +1,8 @@
+// November 18-27 for this week I started planning for a new project. i didnt code this week.
+
+// November 25-27 i practiced using the graphs more for these 3 days. I am startng to get a hang of using it.
+
+// November 28-30 Thanksgiving so no coding as i was out of time.
+
+// December 1-5 preparing for the assessment
+
